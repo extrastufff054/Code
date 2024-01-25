562b4b060b6a8e7f502162f47ccb1e724679db10
extrastufff054 <extrastuff054@gmail.com>
2024-01-25 23:33:55 +0530

    25-01-2024

--- social_media_application/lib/components/my_list_tile.dart
+++ social_media_application/lib/components/my_list_tile.dart
@@ -42,6 +42,6 @@ class MyListTile extends StatelessWidget {
             ),
           ),
         ),
-      );;
+      );
   }
 }
--- social_media_application/lib/pages/users_page.dart
+++ social_media_application/lib/pages/users_page.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_application/components/my_back_button.dart';
 import 'package:social_media_application/components/my_list_tile.dart';
